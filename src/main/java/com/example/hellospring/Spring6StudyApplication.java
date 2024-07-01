090f8872778cc713ae442b9587635c9bc488d9b0
심현섭 <hyunseob.shim@oliveyoung.co.kr>
2024-07-01 20:13:50 +0900

    initial commit

---
+++ src/main/java/com/example/hellospring/Spring6StudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6StudyApplication.class, args);
+	}
+
+}
