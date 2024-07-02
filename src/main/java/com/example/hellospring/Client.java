10a3eee91dc69cd8f720d1751f376f5064701d28
심현섭 <orolsyeo@gmail.com>
2024-07-02 12:32:47 +0900

    object factory to spring

--- src/main/java/com/example/hellospring/Client.java
+++ src/main/java/com/example/hellospring/Client.java
@@ -1,12 +1,15 @@
 package com.example.hellospring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.math.BigDecimal;
 
 public class Client {
 	
 	public static void main(String[] args) throws Exception {
-		ObjectFactory objectFactory = new ObjectFactory();
-		PaymentService paymentService = objectFactory.paymentService();
+		BeanFactory factory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+		PaymentService paymentService = factory.getBean(PaymentService.class);
 		Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
 		System.out.println(payment);
 		
